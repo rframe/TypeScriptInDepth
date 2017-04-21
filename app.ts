313bf3418f6f43ce1c0015e67b58ab9d47f0f63f
Russell F <Russell.Frame@machiningcloud.com>
2017-04-21 15:31:07 -0700

    Arrow Functions

--- app.ts
+++ app.ts
@@ -3,15 +3,15 @@
  */
 
 class Book {
-    constructor(public title: string, public author: string, public available: boolean, public categrory: Category) {
+    constructor(public id: number, public title: string, public author: string, public available: boolean, public categrory: Category) {
     }
 }
 function GetAllBooks(): Array<Book> {
     let books: Array<Book> = [
-        new Book('Ulysses', 'James Joyce', true, Category.Fiction),
-        new Book('A Farewell to Arms', 'Ernest Hemingway', false, Category.Fiction),
-        new Book('I Know Why the Caged Bird Sings', 'Maya Angelou', true, Category.Poetry),
-        new Book('Moby Dick', 'Herman Melville', true, Category.Fiction)
+        new Book(1, 'Ulysses', 'James Joyce', true, Category.Fiction),
+        new Book(2, 'A Farewell to Arms', 'Ernest Hemingway', false, Category.Fiction),
+        new Book(3, 'I Know Why the Caged Bird Sings', 'Maya Angelou', true, Category.Poetry),
+        new Book(4, 'Moby Dick', 'Herman Melville', true, Category.Fiction)
     ];
     return books;
 }
@@ -53,10 +53,16 @@ function LogBookTitles(titles: Array<string>) {
         console.log(title);
     }
 }
-
+function GetBookById(id: number): Book {
+    const allBooks: Array<Book> = GetAllBooks();
+    
+    return allBooks.filter(book => book.id === id)[0];
+}
 
 //const allBooks: Array<Book> = GetAllBooks();
 //LogFirstAvailable(allBooks);
 
-const poetryBooks: Array<string> = GetBookTitlesByCategory(Category.Poetry);
-LogBookTitles(poetryBooks);
+const fictionBooks: Array<string> = GetBookTitlesByCategory(Category.Fiction);
+//LogBookTitles(fictionBooks);
+fictionBooks.forEach((val, idx, arr) => console.log(`${++idx} - ${val}`));
+
