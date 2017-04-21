c7072ee4b6cfcc42a9af4689cb1f5ab41e09c31f
Russell F <Russell.Frame@machiningcloud.com>
2017-04-21 15:14:08 -0700

    TypeScriptBasics: Declaring Enums and Arrays

--- app.ts
+++ app.ts
@@ -3,13 +3,15 @@
  */
 
 class Book {
-    constructor(public title: string, public author: string, public available: boolean){}
+    constructor(public title: string, public author: string, public available: boolean, public categrory: Category) {
+    }
 }
 function GetAllBooks(): Array<Book> {
     let books: Array<Book> = [
-        new Book('Ulysses', 'James Joyce', true),
-        new Book('A Farewell to Arms', 'Ernest Hemingway', false),
-        new Book('I Know Why the Caged Bird Sings', 'Maya Angelou', true)
+        new Book('Ulysses', 'James Joyce', true, Category.Fiction),
+        new Book('A Farewell to Arms', 'Ernest Hemingway', false, Category.Fiction),
+        new Book('I Know Why the Caged Bird Sings', 'Maya Angelou', true, Category.Poetry),
+        new Book('Moby Dick', 'Herman Melville', true, Category.Fiction)
     ];
     return books;
 }
@@ -18,8 +20,8 @@ function LogFirstAvailable(books: Array<Book>): void {
     let numberOfBooks: number = books.length;
     let firstAvailable: string;
 
-    for(let currentBook of books) {
-        if(currentBook.available) {
+    for (let currentBook of books) {
+        if (currentBook.available) {
             firstAvailable = currentBook.title;
             break;
         }
@@ -29,7 +31,32 @@ function LogFirstAvailable(books: Array<Book>): void {
     console.log(`First Available: ${firstAvailable}`)
 }
 
-let allBooks: Array<Book> = GetAllBooks();
+enum Category {Biography, Poetry, Fiction, History, Children};
 
-allBooks.push(new Book('I Know Why the Caged Bird Sings', 'Maya Angelou', true));
-LogFirstAvailable(allBooks);
+function GetBookTitlesByCategory(categoryFilter: Category): Array<string> {
+    console.log(`Getting books in categroy: ${Category[categoryFilter]}`);
+
+    const allBooks = GetAllBooks();
+    const filteredTitles: Array<string> = [];
+
+    for(let currentBook of allBooks) {
+        if(currentBook.categrory === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+
+    return filteredTitles;
+}
+
+function LogBookTitles(titles: Array<string>) {
+    for (let title of titles) {
+        console.log(title);
+    }
+}
+
+
+//const allBooks: Array<Book> = GetAllBooks();
+//LogFirstAvailable(allBooks);
+
+const poetryBooks: Array<string> = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(poetryBooks);
